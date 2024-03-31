3cce531db455d820024929e844738c9cd8aa9c5b
Student <andreysafronoff07@gmail.com>
2024-03-31 18:55:53 +0700

    add Readme

--- djangoIDK/djangoApp/models.py
+++ djangoIDK/djangoApp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class NoteModel(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=51)
     content = models.TextField()
     date_time = models.DateTimeField(auto_now_add=True)
